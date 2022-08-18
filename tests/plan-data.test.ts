38c6f27cd353fbeb798ce693c974c9fe0a28cb34
Jeremy Beightol <jbeightol@digitalocean.com>
2022-08-18 13:43:23 -0400

    converted planitemfactory test to fast-check

--- tests/plan-data.test.ts
+++ tests/plan-data.test.ts
@@ -1,33 +1,60 @@
 import 'mocha';
+import fc from 'fast-check';
 import { expect } from 'chai';
 
 import { PlanItemFactory } from '../src/plan-data';
 import { DayPlannerSettings } from '../src/settings';
+import { BREAK_LABEL, END_LABEL } from '../src/constants';
 
 describe('plan-data', () => {
   describe('PlanItemFactory', () => {
-    const matchIndex = 1;
-    const charIndex = 0;
-    const isBreak = false;
-    const isEnd = false;
-    const time = new Date('2021-04-11T11:10:00.507Z');
-    const rawTime = '11:10';
-    const text = 'meeting';
-    const raw = '- 11:10 meeting';
+    it('should generate PlanItem with given values', () => {
+      fc.assert(
+        fc.property(
+            fc.integer(),
+            fc.integer(),
+            fc.date(),
+            fc.string(),
+            fc.string(),
+            (matchIndex: number, charIndex: number, time: Date, rawTime: string, text: string) => {
+              const factory = new PlanItemFactory(new DayPlannerSettings());
+              const item = factory.getPlanItem(matchIndex, charIndex, false, false, time, rawTime, text, text);
+              expect(item.matchIndex).to.eql(matchIndex);
+              expect(item.charIndex).to.eql(charIndex);
+              expect(item.time).to.eql(time);
+              expect(item.rawTime).to.eql(rawTime);
+              expect(item.text).to.eql(text);
+              expect(item.raw).to.eql(text);
+            }
+        )
+      );
+    });
 
-    it('should generate PlanItem with given text', () => {
-      const factory = new PlanItemFactory(new DayPlannerSettings());
-
-      const item = factory.getPlanItem(matchIndex, charIndex, isBreak, isEnd, time, rawTime, text, raw);
-
-      expect(item.matchIndex).to.eql(matchIndex);
-      expect(item.charIndex).to.eql(charIndex);
-      expect(item.isBreak).to.eql(isBreak);
-      expect(item.isEnd).to.eql(isEnd);
-      expect(item.time).to.eql(time);
-      expect(item.rawTime).to.eql(rawTime);
-      expect(item.text).to.eql(text);
-      expect(item.raw).to.eql(raw);
+    it('should generate PlanItem with break label or end label text when break or end', () => {
+      fc.assert(
+        fc.property(
+            fc.tuple(fc.boolean(), fc.boolean()).filter((i: [ boolean, boolean]) => !i[0] && !i[1]),
+            fc.string(),
+            (breakAndEnd: [boolean, boolean], text: string) => {
+              var isBreak = breakAndEnd[0];
+              var isEnd = breakAndEnd[1];
+              const factory = new PlanItemFactory(new DayPlannerSettings());
+              const item = factory.getPlanItem(0, 0, isBreak, isEnd, new Date(), "", text, text);
+              expect(item.raw).to.eql(text);
+              if (!isBreak && !isEnd) {
+                expect(item.text).to.eql(text);
+              }
+              if (isBreak) {
+                expect(item.text).to.eql(BREAK_LABEL);
+                expect(item.text).to.not.eql(text);
+              }
+              if (isEnd) {
+                expect(item.text).to.eql(END_LABEL);
+                expect(item.text).to.not.eql(text);
+              }
+            }
+        )
+      );
     });
   });
 
